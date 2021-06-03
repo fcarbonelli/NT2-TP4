20f3b12e0a613d0878c2805120fe77c93864d87f
Francisco Carbonelli <carbonelli.francisco@gmail.com>
2021-06-03 16:27:54 -0300

    Resolucion del TP6 agregada

--- src/components/Formulario/src/components/Formulario.js
+++ src/components/Formulario/src/components/Formulario.js
@@ -8,7 +8,8 @@ export default {
       formData : this.getInicialData(),
       formState : {},
       nombreLengthMax: 15,
-      listaUsuarios: []
+      listaUsuarios: [],
+      url: "https://60aec1525b8c300017deb313.mockapi.io/nombre/"
     }
   },
   computed: {
@@ -29,6 +30,28 @@ export default {
       this.listaUsuarios.push(this.formData)
       this.formData = this.getInicialData()
       this.formState._reset()    
+    },
+    async getUsuarios(){
+      try{
+        let respuesta = await this.axios(this.url)
+        console.log("axios get usuarios", respuesta.data)
+        this.listaUsuarios= respuesta.data
+      }
+      catch(error){
+        console.error(error)
+      }
+    },
+    async postUsuario(){
+      try{
+        let respuesta = await this.axios.post(this.url, this.formData, {"content-type": "application/json"})
+        console.log("axios post usuarios", respuesta.data)
+        this.listaUsuarios.push(respuesta.data)
+      }
+      catch(error){
+        console.error(error)
+      }
+      this.formData = this.getInicialData()
+      this.formState._reset()   
     }
   }
 }
