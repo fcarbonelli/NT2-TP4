228669656506a8d4bb62a7c855ad07a9573dc71b
Francisco Carbonelli <carbonelli.francisco@gmail.com>
2021-06-10 21:34:01 -0300

    Entrega

--- src/components/Formulario/src/components/Formulario.js
+++ src/components/Formulario/src/components/Formulario.js
@@ -17,13 +17,12 @@ export default {
     }
   },
   computed: {
-    promedio: function(){
-      var total = 0;
-      for(var i = 0; i < this.listaNotas.nota.length; i++) {
-          total += this.listaNotas.nota[i];
-      }
-      var avg = total / this.listaNotas.nota;
-      return avg
+    promedio: function () {
+      let sum = 0;
+      this.listaNotas.nota.forEach(n => {
+        sum += n.nota;
+      });
+      return sum / this.listaNotas.nota.length;
     }
   },
   mounted () {
@@ -39,27 +38,10 @@ export default {
     },
     enviar(){
       this.listaNotas.push(this.formData)
-      this.setColor(this.formData.nota)
       this.formData = this.getInicialData()
       this.formState._reset()    
     },  
-    setColor(nota)
-    {
-      if (nota >= 0 && nota <=3) {
-        this.notaRoja = true;
-        this.notaVerde = false;
-        this.notaAmarilla = false;
-      }
-      if(nota >= 4 && nota <=6) {
-        this.notaRoja = false;
-        this.notaVerde = false;
-        this.notaAmarilla = true;
-      }if(nota >= 7 && nota <=10) {
-        this.notaRoja = false;
-        this.notaVerde = true;
-        this.notaAmarilla = false;
-      }
-    }
+    
     
   }
 }
